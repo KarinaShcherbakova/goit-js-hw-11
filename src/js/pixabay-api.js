b925f5ed5bd1f3702c10cba4d77b4d1398575275
Karina_Shcherbakova <karina.shcherbakova18@gmail.com>
2024-09-27 16:42:33 +0200

    modified html, css and js files

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,15 @@
+const API_KEY = '46206766-d6f3ed66bf4f64987a1edf465';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export async function fetchImages(query, page = 1, perPage = 12) {
+  const url = `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error('Network response was not ok');
+    return await response.json();
+  } catch (error) {
+    console.error('Fetching images failed:', error);
+    throw error;
+  }
+}
